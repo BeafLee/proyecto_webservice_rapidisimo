7a519517a5748cbf104440456c0355691dfd6aec
faviocpz <favioperez036@gmail.com>
2023-11-27 01:50:28 -0500

    Favio

--- models/vehiculo.py
+++ models/vehiculo.py
@@ -9,3 +9,42 @@ class Vehiculo:
         self.tipo_carga = tipo_carga
         self.modelo = modelo
         self.marca = marca
+
+    def listadoVehiculos(self):
+        con = db().open
+
+        cursor = con.cursor()
+
+        sql = """
+            SELECT * 
+            FROM VEHICULO
+            """
+        
+        cursor.execute(sql)
+
+        datos = cursor.fetchall()
+
+        cursor.close()
+        con.close()
+
+        if datos:
+            return json.dumps({'status': True, 'data': datos, 'message': 'Lista de los vehículos'})
+        else:
+            return json.dumps({'status': False, 'data': [], 'message': 'Sin registros'})
+
+    def registrarVehiculo(self):
+        con=db().open 
+        con.autocommit=False
+        cursor=con.cursor()
+        sql="""insert into vehiculo (matricula, capacidadTotal, tipoCarga, modelo, marca) 
+                    values(%s,%s,%s,%s,%s)"""
+        try:
+            cursor.execute(sql,[self.matricula,self.capacidad_total,self.tipo_carga,self.modelo,self.marca])
+            con.commit()
+            return json.dumps({'status':True,'data':None,'message':'Vehículo registrado correctamente'})
+        except con.Error as error:
+            con.rollback()
+            return json.dumps({'status':False,'data':None,'message':format(error)})
+        finally:
+            cursor.close()
+            con.close()
